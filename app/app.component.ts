da538322291bd821ec412831a7abe43c40862b34
mifaer <mifaer@gmail.com>
2017-01-13 09:40:54 +0200

    todo sketch

--- app/app.component.ts
+++ app/app.component.ts
@@ -1,8 +1,10 @@
 import { Component } from '@angular/core';
 
 @Component({
+  moduleId: module.id,
   selector: 'my-app',
-  template: `<h1>{{title}}</h1>`,
+  templateUrl: './app.component.html',
+  styleUrls: ['app.component.css']
 })
 export class AppComponent  {
   title: string = 'Diary panel for everything...';
---
+++ app/todo/todo.ts
@@ -0,0 +1,32 @@
+import { Component } from '@angular/core';
+
+@Component({
+  moduleId: module.id,
+  selector: 'my-todo',
+  templateUrl: './todo.html',
+  styleUrls: ['todo.css']
+})
+export class TodoComponent  {
+  private todoList: any = [{
+    title: 'Купить продукты',
+    state: true,
+    date: new Date(),
+    id: '1'
+  }, {
+    title: 'Убрать хату',
+    state: false,
+    date: new Date(),
+    id: '2'
+  }, {
+    title: 'Чета еще...',
+    state: true,
+    date: new Date(),
+    id: '3'
+  }
+  ];
+
+  private deleteTodo(todo: any) {
+    console.log(todo);
+    this.todoList = this.todoList.filter((item: any) => item.id !== todo.id);
+  }
+}
